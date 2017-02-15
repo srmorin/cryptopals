a24c7808b0f5ee676c4be67417c7c87c0049d79d
Steve Morin <srmorin@gmzil.com>
2017-02-14 22:16:38 -0500

    change directory hierarchy

--- Set1/1/1.py
+++ Set1/1/1.py
@@ -2,6 +2,7 @@ from utils.consts import base64chars
 from utils.conversion import HexString
 from utils.base64 import base64string
 
+
 def main():
     exercise_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
     expected_string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
