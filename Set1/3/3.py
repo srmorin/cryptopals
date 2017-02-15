414942e4b62aa37f153e7e4c40d7ebed59a8db46
Steve Morin <srmorin@gmzil.com>
2017-02-15 03:22:00 -0500

    add basic analyzer function, minor bug fix

--- Set1/3/3.py
+++ Set1/3/3.py
@@ -1,20 +1,55 @@
 from utils.conversion import HexString
 from utils.conversion import ByteArray
+from utils.consts import english_alphabet_letters
+
+
+def analyze_text(decoded_string):
+    """
+    Scores a decoded string to see the likelihood that it contains english words.
+
+    Very basic function that scores phrases based on the number of characters are valid.
+    Not a great approach, but it found the one I was looking for on the first shot.
+    :param decoded_string: byte array
+    :return: dictionary or score
+    """
+    #TODO(srmorin): create a better analyzer that does more than count decoded characters
+
+    scored_dict = {}
+    total_score = 0
+    for c in range(0, len(decoded_string)):
+        decoded_char = chr(decoded_string[c]).lower()
+        if decoded_char in english_alphabet_letters:
+            total_score += 1
+            try:
+                scored_dict[decoded_char] += 1
+            except KeyError:
+                scored_dict[decoded_char] = 1
+
+    return total_score, scored_dict
 
 
 def main():
     exercise_string_1 = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
     b_exercise_string_1 = HexString.convertToBinaryArray(exercise_string_1)
 
-    #TODO(srmorin): add frequency analysis to guess at specific ones
     #TODO(srmorin): add a function to do the single digit xoring
+    top_score = 0
+    best_phrase = ''
 
-    for i in range(0, 255):
+    for i in range(0, 256):
         newarray = bytearray()
-        for c in range(0, len(b_exercise_string_1)):
-            newarray.append(i ^ b_exercise_string_1[c])
 
-        print '%i: %s' % (i, newarray)
+        for c in range(0, len(b_exercise_string_1)):
+            decoded_c = i ^ b_exercise_string_1[c]
+            newarray.append(decoded_c)
+        total_score, scored_dict = analyze_text(newarray)
+
+        if total_score > top_score:
+            top_score = total_score
+            best_phrase = newarray
+            index = i
+
+    print '%i: %s' % (index, best_phrase)
 
 
 if __name__ == "__main__":
--- utils/consts.py
+++ utils/consts.py
@@ -3,3 +3,18 @@ base64chars = list(chr(x) for x in range(ord('A'), ord('Z') + 1)) + \
               list(chr(x) for x in range(ord('0'), ord('9') + 1)) + \
               list('+') + \
               list('/')
+
+english_alphabet_letters = list(chr(x) for x in range(ord('a'), ord('z') + 1))
+
+# Source for these is wikipedia
+english_letter_frequency = {'e': 12.7, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97, 'n': 6.75, 's': 6.33, 'h': 6.09,
+                            'r': 5.99, 'd': 4.25, 'l': 4.03, 'c': 2.78, 'u': 2.76, 'm': 2.41, 'w': 2.36, 'f': 2.23,
+                            'g': 2.02, 'y': 1.97, 'p': 1.93, 'b': 1.49, 'v': 0.98, 'k': 0.77, 'j': 0.15, 'x': 0.15,
+                            'q': 0.10, 'z': 0.07}
+
+english_letter_frequency_bow = {'a': 11.6, 'b': 4.7, 'c': 3.51, 'd': 2.67, 'e': 2.01, 'f': 3.78, 'g': 1.95, 'h': 7.23,
+                                'i': 6.29, 'j': 0.6, 'k': 0.59, 'l': 2.71, 'm': 4.38, 'n': 2.37, 'o': 6.26, 'p': 2.55,
+                                'q': 0.17, 'r': 1.65, 's': 7.76, 't': 16.67, 'u': 1.49, 'v': 0.65, 'w': 6.75, 'x': 0.02,
+                                'y': 1.62, 'z': 0.03}
+
+
